788ac2f7e8b441c24dfe6b422342b2e81b8bca6f
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-28 10:23:19 -0300

    Update Keyboard.tsx

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,9 +1,74 @@
 import styled from "styled-components";
+import styles from "./Keyboard.module.css";
 
-function Keyboard() {
-  return <KeyboardContainer></KeyboardContainer>;
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+type KeyboardProps = {
+  disabled?: boolean;
+  activeLetters: string[];
+  inactiveLetters: string[];
+  addGuessedLetter: (letter: string) => void;
+};
+
+function Keyboard({
+  activeLetters,
+  inactiveLetters,
+  addGuessedLetter,
+  disabled = false,
+}: KeyboardProps) {
+  return (
+    <KeyboardContainer>
+      {KEYS.map((key) => {
+        const isActive = activeLetters.includes(key);
+        const isInactive = inactiveLetters.includes(key);
+        return (
+          <button
+            onClick={() => addGuessedLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${
+              isInactive ? styles.inactive : ""
+            }`}
+            disabled={isInactive || isActive || disabled}
+            key={key}
+          >
+            {key}
+          </button>
+        );
+      })}
+    </KeyboardContainer>
+  );
 }
 
-const KeyboardContainer = styled.div``;
+const KeyboardContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(75px, 1fr));
+  gap: 0.5rem;
+`;
 
 export default Keyboard;
