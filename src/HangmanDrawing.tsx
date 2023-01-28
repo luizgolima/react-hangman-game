cd0a5677cd28730ce87bd2a1cd3eae5953f8f695
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-28 09:47:21 -0300

    Update HangmanDrawing.tsx

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,9 +1,141 @@
 import styled from "styled-components";
 
-function HangmanDrawing() {
-  return <HangmanDrawingContainer></HangmanDrawingContainer>;
+const HEAD = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
+);
+
+const BODY = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      background: "black",
+      position: "absolute",
+      top: "120px",
+      right: 0,
+    }}
+  />
+);
+
+const RIGHT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "-100px",
+      rotate: "-30deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+
+const LEFT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "10px",
+      rotate: "30deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
+
+const RIGHT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "210px",
+      right: "-90px",
+      rotate: "60deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+);
+
+const LEFT_LEG = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "210px",
+      right: 0,
+      rotate: "-60deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+);
+
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProps = {
+  numberOfGuesses: number;
+};
+
+function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
+  return (
+    <HangmanDrawingContainer>
+      {BODY_PARTS.slice(0, numberOfGuesses)}
+      <div id="first-line"></div>
+      <div id="second-line"></div>
+      <div id="third-line"></div>
+      <div id="fourth-line"></div>
+    </HangmanDrawingContainer>
+  );
 }
 
-const HangmanDrawingContainer = styled.div``;
+const HangmanDrawingContainer = styled.div`
+  position: relative;
+
+  #first-line {
+    height: 50px;
+    width: 10px;
+    background-color: black;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+
+  #second-line {
+    height: 10px;
+    width: 200px;
+    background-color: black;
+    margin-left: 120px;
+  }
+
+  #third-line {
+    height: 400px;
+    width: 10px;
+    background-color: black;
+    margin-left: 120px;
+  }
+
+  #fourth-line {
+    height: 10px;
+    width: 250px;
+    background-color: black;
+  }
+`;
 
 export default HangmanDrawing;
