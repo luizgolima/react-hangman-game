5c8b09022a5beb5d4ba4a9b906c20a9d38572511
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-01-28 10:08:50 -0300

    Update HangmanWord.tsx

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,9 +1,45 @@
 import styled from "styled-components";
 
-function HangmanWord() {
-  return <HangmanWordContainer></HangmanWordContainer>;
+type HangmanWordProps = {
+  guessedLetters: string[];
+  wordToGuess: string;
+  reveal?: boolean;
+};
+
+function HangmanWord({
+  guessedLetters,
+  wordToGuess,
+  reveal = false,
+}: HangmanWordProps) {
+  return (
+    <HangmanWordContainer>
+      {wordToGuess.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility:
+                guessedLetters.includes(letter) || reveal
+                  ? "visible"
+                  : "hidden",
+              color:
+                !guessedLetters.includes(letter) && reveal ? "red" : "black",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </HangmanWordContainer>
+  );
 }
 
-const HangmanWordContainer = styled.div``;
+const HangmanWordContainer = styled.div`
+  display: flex;
+  gap: 0.25em;
+  font-size: 6rem;
+  font-weight: bold;
+  text-transform: uppercase;
+  font-family: monospace;
+`;
 
 export default HangmanWord;
